f5065051d29c0cb70023b0999f45a61ed8a7d773
chenmich <403189920@qq.com>
2021-04-07 20:57:11 +0800

    design Quantity_to_unit

---
+++ tests/test_Quantity_op.py
@@ -0,0 +1,40 @@
+import pytest
+from math import isclose
+
+from quantities import length
+from quantities import velocity
+
+def test_Quantity_add_substract():
+    l_type = length.LengthType
+    l1 = length.Length(22, unit=l_type.k_unit)
+    l2 = length.Length(33, unit=l_type.m_unit)    
+    v = velocity.Velocity(3)
+
+    l3 = l1.add(l2)
+    assert l3.q_type == l_type
+    assert l3.unit == l_type.SI_conherent_unit
+    assert isclose(l3.value, 2200.033)
+    with pytest.raises(ValueError):
+        l1.add(None)
+    with pytest.raises(ValueError):
+        l1.add(42)
+    with pytest.raises(ValueError):
+        l1.add(v)
+    
+    l4 = l1.substract(l2)
+    assert l4.q_type == l_type
+    assert l3.unit == l_type.SI_conherent_unit
+    assert isclose(l4.value, 2200-0.033)
+    with pytest.raises(ValueError):
+        l1.substract(None)
+    with pytest.raises(ValueError):
+        l1.substract(42)
+    with pytest.raises(ValueError):
+        l1.substract(v)
+def test_Quantity_to_unit():
+    l = length.Length(22000)
+    l1 = l.to_unit(l.q_type.k_unit)
+
+    assert l.q_type == l.q_type
+    assert isclose(l.value /1000, l1.value)
+    assert l.current_unit == l.q_type.k_unit
