3d206b62d08440ea569a58879bac02986b8723ff
chenmich <403189920@qq.com>
2021-04-13 15:49:59 +0800

    add all the SI base unit

--- quantities/__init__.py
+++ quantities/__init__.py
@@ -1,9 +1,15 @@
 from .units import Unit
 from .quantity import Quantity
 from .quantity_type import QuantityType
-from .length import LengthType, Length, m
+
+from .identity import IdentityType, Identity, IdentityUnit
 from .time import TimeType, Time, s
+from .length import LengthType, Length, m
 from .mass import MassType, Mass, g
+from .electric_current import ElectriCurrent, ElectriCurrentType, A
+from .thermodynamic_temperature import ThermodynamicTemperatureType, ThermodynamicTemperature, K
+from .amount_of_substance import AmountOfSubstanceType, AmountOfSubstance, mole
+from .luminous_intensity import LuminousIntensityType, LuminousIntensity, cd
 from .velocity import VelocityType, Velocity, m_per_s
 from .acceleration import AccelerationType, Acceleration, meter_per_s_s
 from .force import ForceType, Force, N
---
+++ quantities/thermodynamic_temperature.py
@@ -0,0 +1,22 @@
+from .quantity_type import QuantityType
+from .quantity import Quantity
+from .units import Unit
+
+class K(Unit):
+    profile = {
+        "name":"kelvin",
+        "symbol":"K",
+        "express_by_SI_base":"K", 
+        "express_by_SI":"K"
+    }
+
+class ThermodynamicTemperatureType(QuantityType):
+    pri_unit = K
+    SI_conherent_unit = pri_unit
+    @classmethod
+    def regist_type(cls):
+        pass
+
+class ThermodynamicTemperature(Quantity):
+    def __init__(self, value, unit):
+        super().__init__(value, ThermodynamicTemperatureType, unit)
