529fa58ba58c500248f9887115dffd79cc695152
chenmich <403189920@qq.com>
2021-05-01 10:35:28 +0800

    add hectare unit for area

--- quantities/area.py
+++ quantities/area.py
@@ -11,9 +11,23 @@ class square_meter(Unit):
         "express_by_SI":""
     }
 
+class hectare(Unit):
+    profile = {
+        "name":"hectare",
+        "symbol":"ha", 
+        "express_by_SI_base":"m+2", 
+        "express_by_SI":""
+    }
+    @classmethod
+    def to_pri_unit(cls, value):
+        return 1e+4 * value
+    @classmethod
+    def from_pri_unit(cls, value):
+        return value / 1e+4
 class AreaType(QuantityType):
     pri_unit = square_meter
     SI_conherent_unit = pri_unit
+    hectare = hectare
     @classmethod
     def register_type(cls):
         cls.source[(LengthType, '*', LengthType)] = cls
