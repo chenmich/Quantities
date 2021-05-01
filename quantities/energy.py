3c158ee49022f8e074265f789e0d092804640603
chenmich <403189920@qq.com>
2021-05-01 10:54:10 +0800

    add electronvolt unit for energy

--- quantities/energy.py
+++ quantities/energy.py
@@ -13,13 +13,27 @@ class joule(Unit):
     profile = {
         "name":"joule",
         "symbol":"J",
-        "express_by_SI_base":"kg*m+2*s-2 ",
+        "express_by_SI_base":"kg*m+2*s-2",
         "express_by_SI":"N*m"
     }
+class electronvolt(Unit):
+    profile = {
+        "name":"electronvolt",
+        "symbol":"eV",
+        "express_by_SI_base":"",
+        "express_by_SI":""
+    }
+    @classmethod
+    def to_pri_unit(cls, value):
+        return value * 1.602176634e-19
+    @classmethod
+    def from_pri_unit(cls, value):
+        return value / 1.602176634e-19
 
 class EnergyType(QuantityType):
     pri_unit = joule
     SI_conherent_unit = pri_unit
+    electronvolt = electronvolt
     @classmethod
     def register_type(cls):
         cls.source[(ForceType, '*', LengthType)] = cls
