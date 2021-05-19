94a1303cd74f5fb6945d8afa59782565f03da00c
chenmich <403189920@qq.com>
2021-05-19 09:41:50 +0800

    add two unit for mass quantity type

--- quantities/mass.py
+++ quantities/mass.py
@@ -35,11 +35,38 @@ class dalton(Unit):
     @classmethod
     def from_pri_unit(cls, value):
         return value / 1.66053906660e-24
-
+class pound(Unit):
+    profile = {
+        "name":"pound",
+        "symbol":"lbs",
+        "express_by_SI_base":"",
+        "express_by_SI":""
+    }
+    @classmethod
+    def to_pri_unit(cls, value):
+        return 453.5924 * value
+    @classmethod
+    def from_pri_unit(cls, value):
+        return value / 453.5924
+class ounce(Unit):
+    profile = {
+        "name":"ounce",
+        "symbol":"oz",
+        "express_by_SI_base":"",
+        "express_by_SI":""
+    }
+    @classmethod
+    def to_pri_unit(cls, value):
+        return 28.349556839727 * value
+    @classmethod
+    def from_pri_unit(cls, value):
+        return value / 28.349556839727
 class MassType(QuantityType):
     pri_unit = g
     tonne = tonne
     dalton = dalton
+    pound = pound
+    ounce = ounce
     @classmethod
     def register_type(cls):
         cls.SI_conherent_unit = cls.k_unit
